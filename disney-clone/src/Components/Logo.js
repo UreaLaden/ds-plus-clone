eb41f9800a9d437dd010254109ebb70efff52d04
Leaundrae Mckinney <pytechdrae@gmail.com>
2022-02-20 15:12:59 -0500

    Made from page

---
+++ disney-clone/src/Components/Logo.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const CTALogoOne = styled.img`
+    margin-bottom:12px;
+    max-width:600px;
+    min-height:1px;
+    display:block;
+    width:100%;
+`
+export default CTALogoOne;
